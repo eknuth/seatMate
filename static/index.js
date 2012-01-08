9a3759fbdec6b534a4fa164d8f7f7614f378c218
Edwin Knuth <eknuth@gmail.com>
2012-01-08 11:17:14 -0800

    adding mockups for other tabs

--- static/index.js
+++ static/index.js
@@ -5,56 +5,66 @@ Ext.setup({
     phoneStartupScreen: 'img/phone_startup.png',
     onReady: function() {
         Ext.regModel('Comment', {
-            fields: [
-                { name: 'comment',type: 'string' }
-            ]
+            fields: [{
+                name: 'comment',
+                type: 'string'
+            }]
         });
-       var formBase = {
+        var comments = [{
+          "from_user": "hipster trash",
+          "text": "what's up?",
+          "profile_image_url": "http://placekitten.com/48/48"
+        }];
+        var timeline = new Ext.Component({
+            title: 'Timeline',
+            cls: 'timeline',
             scroll: 'vertical',
-            url   : '/postComment',
-            standardSubmit : false,
-            items: [{
-                    xtype: 'fieldset',
-                    title: 'leave a comment',
-                    instructions: 'comment will be seen by other riders of the 14',
-                    defaults: {
-                        required: true,
-                        labelAlign: 'left',
-                        labelWidth: '40%'
-                    },
-                    items: [
-                    {
-                        xtype: 'textfield',
-                        name : 'comment',
-                        label: 'comment',
-                        useClearIcon: true,
-                        autoCapitalize : false
-                    }]
-                }],
-            dockedItems: [
+            tpl: ['<tpl for=".">',
+                    '<div class="tweet">',
+                    '<div class="avatar"><img src="{profile_image_url}" /></div>',
+                    '<div class="tweet-content">',
+                    '<h2>{from_user}</h2>',
+                    '<p>{text}</p>',
+                    '</div>', '</div>', '</tpl>']
+        });
+        timeline.update(comments);
+        var formBase = {
+            title: 'comment',
+            scroll: 'vertical',
+            url: '/postComment',
+            standardSubmit: false,
+            items: [
+                timeline,
                 {
-                    xtype: 'toolbar',
-                    dock: 'bottom',
-                    items: [
-                        {xtype: 'spacer'},
-                        {
-                            text: 'clear',
-                            handler: function() {
-                                form.reset();
-                            }
-                        },
-                        {
-                            text: 'comment',
-                            ui: 'confirm',
-                            handler: function() {
-                                debugger;
-                                alert(formBase.comment);
-
-                            }
-                        }
-                    ]
-                }
-            ]
+                xtype: 'fieldset',
+                title: 'leave a comment',
+                instructions: 'comment will be seen by other riders of the 14',
+                defaults: {
+                    required: false,
+                    labelAlign: 'left',
+                    labelWidth: '25%'
+                },
+                items: [{
+                    xtype: 'textfield',
+                    name: 'comment',
+                    label: 'comment',
+                    useClearIcon: true,
+                    autoCapitalize: false
+                }]
+            }],
+            dockedItems: [{
+                xtype: 'toolbar',
+                dock: 'bottom',
+                items: [{
+                    xtype: 'spacer'
+                }, {
+                    text: 'comment',
+                    ui: 'confirm',
+                    handler: function() {
+                        alert(formBase.comment);
+                    }
+                }]
+            }]
         };
 
         // form = new Ext.form.FormPanel(formBase);
@@ -65,10 +75,6 @@ Ext.setup({
             sortable: true,
             items: [formBase,
             {
-                title: 'comment',
-                html: 'comment',
-                cls: 'card1'
-            }, {
                 title: 'chat',
                 html: 'chat',
                 cls: 'card2'
