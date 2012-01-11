b314593259f44e3b69a0ac82546b305c8504e093
Edwin Knuth <eknuth@gmail.com>
2012-01-10 21:50:47 -0800

    dispatching comment field to model save method

--- static/index.js
+++ static/index.js
@@ -1,41 +1,69 @@
+var App = new Ext.Application({
+    name: "seatMate"
+});
+
 Ext.setup({
     icon: 'img/icon.png',
     glossOnIcon: false,
     tabletStartupScreen: 'img/tablet_startup.png',
     phoneStartupScreen: 'img/phone_startup.png',
     onReady: function() {
-        Ext.regModel('Comment', {
+        App.models.Comment = Ext.regModel('Comment', {
             fields: [{
+                name: 'from_user',
+                type: 'string'
+            },
+            {
                 name: 'comment',
                 type: 'string'
-            }]
+            },
+            {
+                name: 'profile_image_url',
+                type: 'string'
+            }],
+            proxy:
+            {
+                type: 'localstorage',
+                id: 'comments'
+            }
+            
         });
-        var comments = [{
-          "from_user": "hipster trash",
-          "text": "what's up?",
-          "profile_image_url": "http://placekitten.com/48/48"
+        
+        Ext.regController('Comment', {
+            submitComment: function (param) {
+                console.log(param.data.comment);
+           }
+        });
+        var commentData = [{
+            "from_user": "hipster trash",
+            "comment": "My stop was closed :(",
+            "profile_image_url": "http://placekitten.com/48/48"
+        }, {
+            "from_user": "funky redhead",
+            "comment": "I think the bridge is up?",
+            "profile_image_url": "http://placekitten.com/48/48"
+        }, {
+            "from_user": "smokin' grannie",
+            "comment": "What a beautiful day!",
+            "profile_image_url": "http://placekitten.com/48/48"
         }];
-        var timeline = new Ext.Component({
-            title: 'Timeline',
+        var comments = new Ext.Component({
+            title: 'Comments',
             cls: 'timeline',
             scroll: 'vertical',
-            tpl: ['<tpl for=".">',
-                    '<div class="tweet">',
-                    '<div class="avatar"><img src="{profile_image_url}" /></div>',
-                    '<div class="tweet-content">',
-                    '<h2>{from_user}</h2>',
-                    '<p>{text}</p>',
-                    '</div>', '</div>', '</tpl>']
+            tpl: ['<tpl for=".">', '<div class="tweet">', '<div class="avatar"><img src="{profile_image_url}" /></div>', '<div class="tweet-content">', '<h2>{from_user}</h2>', '<p>{comment}</p>', '</div>', '</div>', '</tpl>']
         });
-        timeline.update(comments);
-        var formBase = {
+
+        comments.update(commentData);
+
+        App.form = new Ext.form.FormPanel({
             title: 'comment',
             scroll: 'vertical',
             url: '/postComment',
             standardSubmit: false,
             items: [
-                timeline,
-                {
+            comments,
+            {
                 xtype: 'fieldset',
                 title: 'leave a comment',
                 instructions: 'comment will be seen by other riders of the 14',
@@ -58,22 +86,28 @@ Ext.setup({
                 items: [{
                     xtype: 'spacer'
                 }, {
-                    text: 'comment',
+                    text: 'leave a comment',
                     ui: 'confirm',
                     handler: function() {
-                        alert(formBase.comment);
+                        Ext.dispatch({
+                            controller: 'Comment',
+                            action: 'submitComment',
+                            data: App.form.getValues()
+                        });
+                                                
+                        // form.submit({
+                        //     waitMsg : {message:'Submitting', cls : 'demos-loading'}
+                        // });
                     }
                 }]
             }]
-        };
-
-        // form = new Ext.form.FormPanel(formBase);
+        });
         // form.show();
         new Ext.TabPanel({
             fullscreen: true,
             type: 'dark',
             sortable: true,
-            items: [formBase,
+            items: [App.form,
             {
                 title: 'chat',
                 html: 'chat',
